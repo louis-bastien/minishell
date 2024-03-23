8d963ac52f4a37f1af4e7e26c25f0f94b6d2f020
louis <email@example.com>
2024-03-23 16:07:27 +0100

    small change

--- srcs/builtins/miniunset.c
+++ srcs/builtins/miniunset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniunset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agheredi <agheredi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 10:14:59 by agheredi          #+#    #+#             */
-/*   Updated: 2024/03/14 12:07:12 by agheredi         ###   ########.fr       */
+/*   Updated: 2024/03/23 16:07:21 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,7 @@ int	mini_unset(t_command *cmd, char ***env)
 		index_var = get_var_index(cmd->args[i], *env);
 		if (cmd->args_count >= 2
 			&& index_var != -1)
-		{
 			*env = rm_d_array(*env, index_var);
-		}
 		i++;
 	}
 	return (0);
